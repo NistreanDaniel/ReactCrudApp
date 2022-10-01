030c0dfa0063c00115d92279f75c5686c52c19e5
NistreanDaniel <dan.nistrean@gmail.com>
2022-10-01 16:29:46 +0300

    Finished UI

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import {Home} from './components/Home';
+import {EditUser} from './components/EditUser';
+import {AddUser} from './components/AddUser';
+
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
-    <div className="App">
-      <h1>app</h1>
+    <div style={{maxWidth: "30rem", margin: "4rem auto"}}>
+        <Router>
+            <Routes>
+                <Route exact path="/" element={ <Home />} ></Route>
+                <Route exact path="/add" element={ <AddUser /> } ></Route>
+                <Route exact path="/edit/:id" element={ <EditUser /> } ></Route>
+            </Routes>
+        </Router>
     </div>
   );
 }
